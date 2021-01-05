c98899a9ad50c0f747ba58f9cfedae66ac8a31f5
Romain heloise <romheloise@gmail.com>
2021-01-05 13:10:47 +0100

    css responsive..

--- config/multer.js
+++ config/multer.js
@@ -5,7 +5,7 @@ const storage = multer.memoryStorage();
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 3000000 },
+  limits: { fileSize: 10000000 },
   fileFilter: (req, file, cb) => {       
     const fileType = ["image/jpeg", "image/png"];
     if (!fileType.includes(file.mimetype)) {
